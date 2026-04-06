31944e0582127cb0542dce8229f72be0dd9c02fa
pineapplecat22 <pineapplecat22@canyoujustputanythinghere>
2026-04-06 10:44:31 -0700

    im too tired to program the setter commands

--- src/main/java/com/pineapple/betterpollution/BetterPollutionCommands.java
+++ src/main/java/com/pineapple/betterpollution/BetterPollutionCommands.java
@@ -3,14 +3,14 @@ package com.pineapple.betterpollution;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.context.CommandContext;
+import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.neoforged.fml.common.Mod;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 
 import java.util.Arrays;
-
-import static com.pineapple.betterpollution.BetterPollutionCommon.getPollutionAtPlayer;
 
 @Mod(BetterPollution.MODID)
 public class BetterPollutionCommands {
@@ -19,34 +19,28 @@ public class BetterPollutionCommands {
 
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         // Build the command structure using Commands.literal and Commands.argument
-        dispatcher.register(Commands.literal("mycommand")
-                .requires(source -> source.hasPermission(2)) // Set required permission level
-                .then(Commands.literal("subcommand")
-                        .then(Commands.argument("number", IntegerArgumentType.integer(1, 10)) // Add an argument
-                                .executes(BetterPollutionCommands::execute) // Specify the method to run
-                        )
-                )
-        );
         dispatcher.register((Commands.literal(BetterPollution.MODID)
                 .then(Commands.literal("getPollution")
-                        .executes(BetterPollutionCommands::getPollution)
+                        .executes(ctx -> BetterPollutionCommands.getPollution(ctx))
+                        .then(Commands.argument("pos", BlockPosArgument.blockPos())
+                                .executes(ctx -> BetterPollutionCommands.getPollutionWithPos(ctx, BlockPosArgument.getBlockPos(ctx, "pos"))
+                        )
                 )
-        ));
+        )));
         BetterPollution.LOGGER.debug("BetterPollution commands registered!"); //...probably.
     }
 
-    private static int execute(CommandContext<CommandSourceStack> context) {
-        // Logic to execute when the command is run
-        int number = IntegerArgumentType.getInteger(context, "number");
-        context.getSource().sendSuccess(() -> Component.literal("You entered the number: " + number), false);
-
-        // Return a result value (optional, but standard practice)
-        return number;
+    private static int getPollution(CommandContext<CommandSourceStack> context) {
+        context.getSource().sendSuccess(() -> Component.literal(
+                Arrays.toString(BetterPollutionCommon.getPollutionAtPlayer(context.getSource().getPlayer()))
+        ), false);
+        return 0; //uhhhhhh
     }
 
-    private static int getPollution(CommandContext<CommandSourceStack> context) {
-        context.getSource().sendSuccess(() -> Component.literal(Arrays.toString(getPollutionAtPlayer(context.getSource().getPlayer()))), false);
-        context.getSource().getLevel();
-        return 0; //uhhhhhh
+    private static int getPollutionWithPos(CommandContext<CommandSourceStack> context, BlockPos blockPos) {
+        context.getSource().sendSuccess(() -> Component.literal(Arrays.toString(
+                BetterPollutionCommon.getPollutionAtPos(blockPos, context.getSource().getLevel())
+                )), false);
+        return 0;
     }
 }
