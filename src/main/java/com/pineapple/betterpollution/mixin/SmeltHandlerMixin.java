bbbc0331f4338e4ccb6de5068c4261f676f560b6
pineapplecat22 <pineapplecat22@canyoujustputanythinghere>
2026-04-20 11:51:43 -0700

    last known functioning build, before i break everything

--- src/main/java/com/pineapple/betterpollution/mixin/SmeltHandlerMixin.java
+++ src/main/java/com/pineapple/betterpollution/mixin/SmeltHandlerMixin.java
@@ -1,11 +1,14 @@
 package com.pineapple.betterpollution.mixin;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.NonNullList;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.world.item.ItemStack;
 
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,15 +19,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(AbstractFurnaceBlockEntity.class)
 public class SmeltHandlerMixin {
 
-    @Inject(method = "burn", at = @At("TAIL"))
-    private static void onSmeltComplete(RegistryAccess registryAccess, RecipeHolder<?> recipe, NonNullList<ItemStack> inventory, int maxStackSize, AbstractFurnaceBlockEntity furnace, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "serverTick", at = @At("TAIL"), remap = false)
+    private static void onSmeltComplete(Level level, BlockPos pos, BlockState state, AbstractFurnaceBlockEntity blockEntity, CallbackInfo ci) {
 
         // Slot 2 = output
-        ItemStack output = furnace.getItem(2);
-
-        if (!output.isEmpty()) {
+        ItemStack output = blockEntity.getItem(2);
+        if (false) { //blockEntity.isLit()
             BetterPollution.LOGGER.info("hi! im a furnace");
             BetterPollution.LOGGER.info(output.toString());
         }
     }
 }
+// TODO: GET ACCESS TRANSFORMER WORKING!!!
+// TODO: HOUSEKEEPING: MOVE neoforge.mods.toml OUT OF /templates
